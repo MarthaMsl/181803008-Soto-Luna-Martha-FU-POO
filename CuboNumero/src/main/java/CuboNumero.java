e60489c8aee928af4f33363eb0e03b4c51dc982e
Martha Soto Luna <martha.soto1108@uppuebla.edu.mx>
2020-05-23 00:23:39 -0500

    Calculo de Cubo de un numero

---
+++ CuboNumero/src/main/java/CuboNumero.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CuboNumero{
+    
+
+  
+  public static void main(String args []){
+    Scanner scan=new Scanner (System.in);
+    int numero=0;
+    System.out.println("programa que calcula el cubo de un numero");
+    System.out.println("Teclea un numero");
+    numero=scan.nextInt();
+    System.out.println("El cubo de el numero es " + ((numero*numero)*numero));
+  }
+}
